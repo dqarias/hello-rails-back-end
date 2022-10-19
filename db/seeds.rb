a2c5e082a1bc945aea635c372ab7f0a3f1b98844
dqarias <dino.quispea@gmail.com>
2022-10-18 21:52:56 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create!(message: 'Hello world')
+Greeting.create!(message: 'How is it going')
+Greeting.create!(message: 'How are you doing')
+Greeting.create!(message: 'Good Morning')
+Greeting.create!(message: 'Hi welcome')
